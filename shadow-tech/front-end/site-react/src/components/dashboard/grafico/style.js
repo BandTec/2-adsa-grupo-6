c131226727d107982b20418c1f4ca4ac9fb71b4d
reisrb <rafael.reis@bandtec.com.br>
2020-06-06 21:46:12 -0300

    div de processos

--- shadow-tech/front-end/site-react/src/components/dashboard/grafico/style.js
+++ shadow-tech/front-end/site-react/src/components/dashboard/grafico/style.js
@@ -6,9 +6,11 @@ export const chart = styled.div`
   height: 70vh;
   color: white; 
   background-color: white;
+  margin: 3vh 0;
 
   canvas{
     height: 100%;
+    padding: 1%;
   }
 `;
 
--- shadow-tech/front-end/site-react/src/pages/dashboards/style.js
+++ shadow-tech/front-end/site-react/src/pages/dashboards/style.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 export const dashs = styled.div`
   width: 95%;
-
+  
   div{
-    margin: 4vh 0;
+    border-radius: 5px;
   }
-
 `;
