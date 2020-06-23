8d38e3c8e73551d435c3f0a1815787f3863754f1
reisrb <rafael.reis@bandtec.com.br>
2020-06-22 21:09:12 -0300

    adicionando processos

--- shadow-tech/back-end/Java-JFrame/teste-oshi/src/main/java/com/mycompany/teste/oshi/ProcessosMemoria.java
+++ shadow-tech/back-end/Java-JFrame/teste-oshi/src/main/java/com/mycompany/teste/oshi/ProcessosMemoria.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.teste.oshi;
 
 import java.util.ArrayList;
@@ -9,47 +8,43 @@ import oshi.hardware.HardwareAbstractionLayer;
 import oshi.software.os.OSProcess;
 
 import oshi.software.os.OperatingSystem;
-import oshi.util.FormatUtil;
 
 public class ProcessosMemoria {
-    
+
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        
+
         SystemInfo info = new SystemInfo();
         HardwareAbstractionLayer hal = info.getHardware();
         GlobalMemory memory = hal.getMemory();
         OperatingSystem op = info.getOperatingSystem();
         List ProcessosMemoria = printProcesses(op, memory);
-        System.out.println(ProcessosMemoria);
+//        System.out.println(ProcessosMemoria);
         ProcessosMemoria.forEach((i) -> {
             System.out.println(i + "\n");
         });
     }
-    
- private static List printProcesses(OperatingSystem os, GlobalMemory memory) {
+
+ @SuppressWarnings("unchecked")
+ public static List printProcesses(OperatingSystem os, GlobalMemory memory) {
 
         List oshi = new ArrayList();
 
         OSProcess myProc = os.getProcess(os.getProcessId());
-        
-        oshi.add(
-                "My PID: " + myProc.getProcessID()+ " with affinity " + Long.toBinaryString(myProc.getAffinityMask()));
-        
-        oshi.add("Processes: " + os.getProcessCount() + ", Threads: " + os.getThreadCount()+"\n");
 
-        List<OSProcess> procs = os.getProcesses(30, OperatingSystem.ProcessSort.MEMORY);
-        
-        oshi.add("   PID  %CPU %MEM       VSZ       RSS Name");
-        
-        for (int i = 0; i < procs.size() && i < 30; i++) {
+        List<OSProcess> procs = os.getProcesses(20, OperatingSystem.ProcessSort.CPU);
+
+        oshi.add(" %CPU %MEM   Name");
+
+        for (int i = 0; i < procs.size() && i < 20; i++) {
             OSProcess p = procs.get(i);
-            oshi.add(String.format(" %5d %5.1f %4.1f %9s %9s %s", p.getProcessID(),
+            oshi.add(String.format("%5.1f %2.1f   %s",
                     100d * (p.getKernelTime() + p.getUserTime()) / p.getUpTime(),
-                    100d * p.getResidentSetSize() / memory.getTotal(), FormatUtil.formatBytes(p.getVirtualSize()),
-                    FormatUtil.formatBytes(p.getResidentSetSize()), p.getName()));
+                    100d * p.getResidentSetSize() / memory.getTotal(),
+                    p.getName()));
         }
 
         return oshi;
  }
-    
+
 }
