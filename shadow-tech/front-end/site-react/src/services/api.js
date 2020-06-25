1fbbd2a1bed4cb79bfe8227ebf9a15e1d7f73f88
reisrb <rafael.reis@bandtec.com.br>
2020-06-25 11:24:54 -0300

    configurando ambiente de producao

--- shadow-tech/front-end/site-react/src/services/api.js
+++ shadow-tech/front-end/site-react/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://nodeshadow.azurewebsites.net'
-    baseURL: 'http://localhost:3001'
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://nodeshadow.azurewebsites.net'
 });
 
 export default api;
