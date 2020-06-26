65489118970ec520cedb65271e7a7e963498a690
reisrb <rafael.reis@bandtec.com.br>
2020-06-25 21:07:11 -0300

    arrumando login e cadastro da navbar

--- shadow-tech/front-end/site-react/src/components/navigation-bar/navigation-bar.js
+++ shadow-tech/front-end/site-react/src/components/navigation-bar/navigation-bar.js
@@ -1,69 +1,141 @@
-import React, {useState} from 'react';
-import {useModal} from 'use-react-modal';
-import {useHistory} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useModal } from 'use-react-modal';
+import { useHistory } from 'react-router-dom';
+
+import Loading from '../../components/Loading';
+
 
 import logo from '../../assets/logo.png';
 import logoShiny from '../../assets/logo-shiny.png';
 import * as S from './style';
 
-export default function NavigationBar({ isDash }){
+import api from '../../services/api';
+
+
+export default function NavigationBar({ isDash }) {
 
     const hist = useHistory();
+    let [isLoading, setIsLoading] = useState(false);
+
 
     const [mostrarDropdown, setMostrarDropdown] = useState(false);
     const [mostrarLogoShiny, setMostrarLogoShiny] = useState(false);
-    const [ esqueciSenha, setEsqueciSenha ] = useState(false);
-    
-    const [ abrirModalLogin, fecharModalLogin, isOpenModalLogin, ModalLogin ] = useModal({
+    const [esqueciSenha, setEsqueciSenha] = useState(false);
+
+    const [abrirModalLogin, fecharModalLogin, isOpenModalLogin, ModalLogin] = useModal({
         background: 'rgba(0,0,0,0.3)',
     });
-    const [ abrirModalCadastro, fecharModalCadastro, isOpenCadastro, ModalCadastro ] = useModal({
+    const [abrirModalCadastro, fecharModalCadastro, isOpenCadastro, ModalCadastro] = useModal({
         background: 'rgba(0,0,0,0.3)'
     });
-    const [ abrirModalPerfil, fecharModalPerfil, isOpenPerfil, ModalPerfil ] = useModal({
+    const [abrirModalPerfil, fecharModalPerfil, isOpenPerfil, ModalPerfil] = useModal({
         background: 'rgba(0,0,0,0.3)'
     });
 
-    const sair = () =>{
+    const sair = () => {
         sessionStorage.clear();
 
         hist.push('/');
     }
 
-    return(
+
+    async function handleLogin() {
+        setIsLoading(true);
+
+        let email = document.getElementById('txtEmail').value;
+        let senha = document.getElementById('txtSenha').value;
+
+
+        await api.post('/userLogin', { "email": email, "senha": senha }).then(res => {
+            setIsLoading(false);
+            fecharModalLogin();
+            hist.push('/dashboards');
+        }).catch(error => {
+            setIsLoading(false);
+            if (error.response.status === 404) {
+                alert('Credenciais inválidas!');
+            } else {
+                alert('Problema de conexão!');
+            }
+        });
+    }
+
+    async function handleRegister() {
+        setIsLoading(true);
+        let nome = document.getElementById('txtNome').value;
+        let email = document.getElementById('txtEmail').value;
+        let senha = document.getElementById('txtSenha').value;
+        let confirmarSenha = document.getElementById('txtConfirmaSenha').value;
+
+        await api.post('/listEmail', { "email": email }).then(res => {
+            setIsLoading(false);
+            alert('Usuário já cadastrado');
+        }).catch(async (error) => {
+            setIsLoading(false);
+            if (error.response.status === 404) {
+                if (nome !== "" &&
+                    email !== "" &&
+                    senha !== "") {
+                    if (senha.length > 5) {
+                        if (senha === confirmarSenha) {
+                            if (email.endsWith('@bandtec.com.br') && email !== '@bandtec.com.br') {
+                                await api.post('/user', { "nome": nome, "email": email, "senha": senha }).then(function (res) {
+                                    alert('Usuario cadastrado com sucesso');
+                                }).catch(function (error) {
+                                    alert('Problema de conexão!');
+                                });
+                            } else {
+                                alert('Seu email não pertence a nenhuma instituição cadastrada')
+                            }
+                        } else {
+                            alert('Senhas não compativeis ');
+                        }
+                    } else {
+                        alert('Senha deve conter no minimo 6 caracteres');
+                    }
+                } else {
+                    alert('Por favor , não deixar nenhum campo vazio');
+                }
+            } else {
+                alert('Sem conexão com o banco')
+            }
+        })
+    }
+
+    return (
         <>
             <S.NavigationBarContainer>
                 <S.NavigationBar>
                     <S.LogoContainer>
-                    <div onMouseEnter={() => setMostrarLogoShiny(true)}
-                        onMouseLeave={() => setMostrarLogoShiny(false)}>
-                        { mostrarLogoShiny  ?  <S.LogoImagem src={logoShiny} onClick={() => isDash ? "" : alert('opa')} alt=""/>  :  <S.LogoImagem src={logo} /> }
-                    </div>
+                        <div onMouseEnter={() => setMostrarLogoShiny(true)}
+                            onMouseLeave={() => setMostrarLogoShiny(false)}>
+                            {mostrarLogoShiny ? <S.LogoImagem src={logoShiny} onClick={() => isDash ? "" : alert('opa')} alt="" /> : <S.LogoImagem src={logo} />}
+                        </div>
                     </S.LogoContainer>
                     <S.ButtonsContainer>
-                    {isDash ?
-                        <>
-                        <S.Button onClick={() => hist.push('/dashboards')}>Dashboard</S.Button>
-                        <S.Button onClick={() => hist.push('/alunosProcesso')}>Aluno</S.Button>
-                        <S.Button>Suporte</S.Button>
-                        </>
-                        : 
-                        <> 
-                        <S.Button>Produto</S.Button>
-                        <S.Button>Sobre</S.Button>
-                        <S.Button>Equipe</S.Button>
-                        </>}
-                        <S.Button 
-                        onMouseEnter={() => setMostrarDropdown(true)}
-                        onMouseLeave={() => setMostrarDropdown(false)}>
+                        {isDash ?
+                            <>
+                                <S.Button onClick={() => hist.push('/dashboards')}>Dashboard</S.Button>
+                                <S.Button onClick={() => hist.push('/alunosProcesso')}>Aluno</S.Button>
+                                <S.Button>Suporte</S.Button>
+                            </>
+                            :
+                            <>
+                                <S.Button>Produto</S.Button>
+                                <S.Button>Sobre</S.Button>
+                                <S.Button>Equipe</S.Button>
+                            </>}
+                        <S.Button
+                            onMouseEnter={() => setMostrarDropdown(true)}
+                            onMouseLeave={() => setMostrarDropdown(false)}>
                             Acesso
                         </S.Button>
                     </S.ButtonsContainer>
                 </S.NavigationBar>
                 {mostrarDropdown && (
-                    <S.Dropdown 
-                    onMouseEnter={() => setMostrarDropdown(true)}
-                    onMouseLeave={() => setMostrarDropdown(false)}>
+                    <S.Dropdown
+                        onMouseEnter={() => setMostrarDropdown(true)}
+                        onMouseLeave={() => setMostrarDropdown(false)}>
                         <S.DropdownContainer>
                             <S.DivDropdown>
                                 <S.DropdownOption>
@@ -83,10 +155,11 @@ export default function NavigationBar({ isDash }){
                     </S.Dropdown>
                 )}
             </S.NavigationBarContainer>
-            
+
             {isOpenModalLogin && (
                 <ModalLogin>
                     <S.Modal>
+                        {isLoading ? <Loading /> : null}  
                         <S.DivContainerClose>
                             <S.SpanClose onClick={fecharModalLogin}>
                                 &times;
@@ -99,18 +172,18 @@ export default function NavigationBar({ isDash }){
                         </S.DivTituloModal>
                         <S.ConteudoModal>
                             <S.DivLabelInput>
-                                <S.LabelModal htmlFor="txtEmail">E-mail:</S.LabelModal>
+                                <S.LabelModal type="password" htmlFor="txtEmail">E-mail:</S.LabelModal>
                                 <S.InputModal type="text" id="txtEmail" placeholder="Insira o seu e-mail" />
                             </S.DivLabelInput>
                             <S.DivLabelInput>
                                 <S.LabelModal htmlFor="txtSenha">Senha:</S.LabelModal>
-                                <S.InputModal type="text" id="txtSenha" placeholder="Insira a sua senha" />
+                                <S.InputModal type="password" id="txtSenha" placeholder="Insira a sua senha" />
                             </S.DivLabelInput>
                             <S.DivCheck>
-                                <S.CheckboxModal type="checkbox" id="chkLembrar"/>
+                                <S.CheckboxModal type="checkbox" id="chkLembrar" />
                                 <S.LabelModal htmlFor="chkLembrar">Lembrar de mim</S.LabelModal>
                             </S.DivCheck>
-                            <S.ButtonModal>
+                            <S.ButtonModal onClick={handleLogin}>
                                 ACESSAR
                             </S.ButtonModal>
                             <S.DivEsqueciSenha>
@@ -119,7 +192,7 @@ export default function NavigationBar({ isDash }){
                                 </S.SpanEsqueciSenha>
                             </S.DivEsqueciSenha>
                         </S.ConteudoModal>
-                       
+
                         {esqueciSenha && (
                             <S.ModalEsqueciSenha>
                                 <S.DivContainerClose>
@@ -137,7 +210,7 @@ export default function NavigationBar({ isDash }){
                                         Ao utilizar este formulário, será enviada uma nova senha para o e-mail inserido no campo abaixo.
                                     </S.DescricaoEsqueciSenha>
                                 </S.DivDescricaoSenha>
-                                <S.ConteudoModal style={{paddingTop: '0'}}>
+                                <S.ConteudoModal style={{ paddingTop: '0' }}>
                                     <S.DivLabelInput>
                                         <S.LabelModal htmlFor="txtEmail">E-mail:</S.LabelModal>
                                         <S.InputModal type="text" id="txtEmail" placeholder="Insira o seu e-mail" />
@@ -152,11 +225,12 @@ export default function NavigationBar({ isDash }){
                     </S.Modal>
                 </ModalLogin>
             )}
-            
-            
+
+
             {isOpenCadastro && (
                 <ModalCadastro>
                     <S.Modal>
+                        {isLoading ? <Loading /> : null}  
                         <S.DivContainerClose>
                             <S.SpanClose onClick={fecharModalCadastro}>
                                 &times;
@@ -185,10 +259,10 @@ export default function NavigationBar({ isDash }){
                                 <S.InputModal type="text" id="txtConfirmaSenha" placeholder="Insira novamente a sua senha" />
                             </S.DivLabelInput>
                             <S.DivCheck>
-                                <S.CheckboxModal type="checkbox" id="chkLembrar"/>
+                                <S.CheckboxModal type="checkbox" id="chkLembrar" />
                                 <S.LabelModal htmlFor="chkLembrar">Receber novidades por e-mail</S.LabelModal>
                             </S.DivCheck>
-                            <S.ButtonModal>
+                            <S.ButtonModal onClick={handleRegister}>
                                 CADASTRAR
                             </S.ButtonModal>
                         </S.ConteudoModal>
@@ -229,7 +303,7 @@ export default function NavigationBar({ isDash }){
                     </S.Modal>
                 </ModalPerfil>
             )}
-            
-        </> 
+
+        </>
     );
 }
